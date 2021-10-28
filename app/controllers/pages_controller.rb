bc3728c71d7144a341771115f2e2248438401f65
ruthgabrielle <ruthfgms@gmail.com>
2021-10-28 10:47:45 -0300

    custom routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,7 @@ class PagesController < ApplicationController
   end
 
   def about
+    @title = "My new page"
   end
 
   def home
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'pages/contact'
-  get 'pages/about'
-  get 'pages/home'
+  get 'contact', to: 'pages#contact'
+  get 'about', to: 'pages#about'
+
   resources :projects
 end
